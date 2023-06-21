870fd7533b46d675a420d202ebb5ff00063c4f52
Alvaro <alvaroph@gmail.com>
2023-06-22 00:54:54 +0200

    mejora tipos - data table no funcional

--- src/components/interfaces.ts
+++ src/components/interfaces.ts
@@ -1,7 +1,22 @@
-export interface Talumnos {
+export interface TActividad {
+    id: number;
+    nombre: string;
+    descripcion: string;
+    uf: number; 
+}
+export interface TAlumnoActividad{
+    id: number;
+    id_alumno: number;
+    id_actividad: TActividad;
+    nota: number;
+
+}
+
+export interface Talumno {
     id: number;
     nombre: string;
     apellido: string;
     curso: string;
+    ALUMNO_ACTIVIDAD: TAlumnoActividad[];
 
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,31 @@
 
   <div v-for="alumno in alumnos" v-bind:key="alumno.id">
     alumno: {{ alumno.id }} - {{ alumno.nombre }}
-    <div v-for="actividad in alumno.ALUMNO_ACTIVIDAD">
-      {{ actividad.nota }}
+    <div v-for="actividad in alumno.ALUMNO_ACTIVIDAD" v-bind:key="actividad.id">
+      {{actividad.id}} - {{ actividad.id_actividad.nombre }} - {{ actividad.nota }}
     </div>
   </div>
+
+  <v-data-table :headers="headers" :items="alumnos">
+    <template v-slot:items="props">
+      <td>{{ props.item.nombre }}</td>
+      </template>
+    
+  </v-data-table>
 </template>
 
 <script lang="ts">
 
   
-  import {Talumnos} from '@/components/interfaces';
+  import {Talumno} from '@/components/interfaces';
 //  import HelloWorld from '@/components/HelloWorld.vue';
   import  { defineComponent } from "vue";
   export default defineComponent({
   data() {
     return {
-      alumnos:  [] as Talumnos[]
+      alumnos:  [] as Talumno[],
+      headers: [{ title: 'Nombre', value: 'nombre' }] 
+
     } }
     ,created() {
       console.log('created')
