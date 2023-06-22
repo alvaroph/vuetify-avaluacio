94197a64c55252c6884c2869bd5864fd78253939
Alvaro <alvaroph@gmail.com>
2023-06-22 10:39:39 +0200

    ruta adicoinal creada

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,20 @@ const routes = [
       },
     ],
   },
+  {
+    path: '/graella',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '/graella',
+        name: 'Graella',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Graella.vue'),
+      },
+    ],
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Graella.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Grilla</h1>
+      <p>Esto es la grilla</p>
+    </div>
+  </template>
+  
+  <script lang="ts" >
+    console.log("Esto es la grilla");
+    
+  </script>
+  
