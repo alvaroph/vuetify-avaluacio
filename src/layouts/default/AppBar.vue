f9572acd8f045bd21dddf3700ee2bb17b972b09c
Alvaro <alvaroph@gmail.com>
2023-06-23 23:15:50 +0200

    cambios menores

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -2,9 +2,13 @@
   <v-app-bar flat>
     <v-app-bar-title>
       <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
-    </v-app-bar-title>
+      <v-btn variant="outlined">
+        <router-link to="/">Portada</router-link>
+    </v-btn>
+    <v-btn variant="outlined">
+      <router-link to="/graella">Graella notes</router-link>
+    </v-btn>
+      </v-app-bar-title>
   </v-app-bar>
 </template>
 
